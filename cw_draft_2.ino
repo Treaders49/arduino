c6332fd978fc2039be8d8f865a493c66e58b66ff
Treaders49 <68151857+Treaders49@users.noreply.github.com>
2022-03-23 19:27:55 +0000

    Add files via upload
    corrected select feature

--- cw_draft_2.ino
+++ cw_draft_2.ino
@@ -11,6 +11,7 @@ void setup() {
 }
 #define SYNC 0
 #define MAIN 1
+#define SELECT 2
 int state = SYNC;
 struct channels{
   char letter;
@@ -32,18 +33,53 @@ void sortLetters() {
     }
     for (int y = 0 ; y < elementIndex - 1; y++) {
       if (channelArray[y].letter > channelArray[y+1].letter) {
-        
-          char temp, temp1, temp2, temp3, temp4  = channelArray[y].letter, channelArray[y].value, channelArray[y].minVal, channelArray[y].letter, channelArray[y].description ; //swapping the values
-          temp1 = channelArray[y].value;
-          temp2 = channelArray[y].minVal;
-          channelArray[y].letter, channelArray[y].value, channelArray[y].minVal, channelArray[y].letter, channelArray[y].description = channelArray[y+1].letter, channelArray[y+1].value, channelArray[y+1].minVal, channelArray[y+1].letter, channelArray[y+1].description ;
-          channelArray[y+1].letter, channelArray[y+1].value, channelArray[y+1].minVal, channelArray[y+1].letter, channelArray[y+1].description  = temp, temp1, temp2, temp3, temp4;
-        
+        channels temp = channelArray[y];
+        channelArray[y] = channelArray[y+1];
+        channelArray[y+1] =  temp;
+          
         
         
       }
       
     } 
+  }
+  
+}
+
+void updateColour() {
+  int smallerMin = 0;
+  int biggerMax = 0;
+  for (int x = 0; x < elementIndex; x++) {
+    
+    Serial.println(channelArray[x].minVal);
+    Serial.println(channelArray[x].maxVal);
+    Serial.println(channelArray[x].value);
+    if (channelArray[x].minVal > channelArray[x].maxVal) {
+      continue;
+    }
+    if (channelArray[x].value > channelArray[x].maxVal) {
+      biggerMax = 1;
+    }
+    if (channelArray[x].value < channelArray[x].minVal) {
+      smallerMin = 1;
+    }
+    Serial.println(smallerMin);
+    Serial.println(biggerMax);
+  }
+   
+   if ((smallerMin == 1) && (biggerMax == 0)) {
+    lcd.setBacklight(2);
+  }
+  if ((biggerMax == 1) && (smallerMin == 0)) {
+    lcd.setBacklight(1);
+  } 
+  if ((smallerMin == 1) && (biggerMax == 1)) {
+    lcd.setBacklight(3);
+  }
+  
+  
+  if ((smallerMin == 0) && (biggerMax == 0)) {
+    lcd.setBacklight(7);
   }
   
 }
@@ -60,17 +96,21 @@ int getIndex(int value) {
   return -1; //denotes letter not found
 }
 void updateDisplay() {
+  lcd.setCursor(0,0);
+  lcd.print("^");
+  lcd.setCursor(0,1);
+  lcd.print("v");
   lcd.setCursor(1,0);
   lcd.print(channelArray[menuPosition].letter);
   lcd.setCursor(2,0);
-  Serial.println();
-  Serial.println(channelArray[menuPosition].value);
   if (channelArray[menuPosition].value < 100) {
-      lcd.print("0"); 
+      lcd.print(" ");
       lcd.setCursor(3,0);
-      if (channelArray[menuPosition].value < 10) {
-          lcd.print("0"); 
+      
+      if (channelArray[menuPosition].value < 10) { 
+          lcd.print(" ");
           lcd.setCursor(4,0);
+          
         }
       
     }
@@ -80,11 +120,12 @@ void updateDisplay() {
     lcd.setCursor(1,1);
     lcd.print(channelArray[menuPosition+1].letter);
     lcd.setCursor(2,1);
-    if (channelArray[menuPosition+1].value < 100) {
-        lcd.print("0"); 
+    if (channelArray[menuPosition+1].value < 100) { 
+        lcd.print(" ");
         lcd.setCursor(3,1);
+        
         if (channelArray[menuPosition+1].value < 10) {
-          lcd.print("0"); 
+          lcd.print(" ");
           lcd.setCursor(4,1);
         }
       }
@@ -98,6 +139,7 @@ void updateDisplay() {
 
 
 void loop() {
+  
   int x, y = 0;
   switch(state) {
     case SYNC: {
@@ -108,7 +150,9 @@ void loop() {
         delay(1000);
       }
      }
-    Serial.print("BASIC");
+     //A placeholder for whatever is inputted so it doesnt give an error
+    Serial.println("");
+    Serial.println("BASIC");
     lcd.setBacklight(7);
     state = MAIN;
    break;
@@ -119,6 +163,20 @@ void loop() {
     lcd.print("^");
     lcd.setCursor(0,1);
     lcd.print("v");
+    updateColour();
+    if (lcd.readButtons() == 1) {
+      
+        state = SELECT;
+        break;
+      }
+      
+      
+    
+    
+    updateColour();
+
+    
+    
     if ((lcd.readButtons() == 4) && (menuPosition < (elementIndex-2))) {
         menuPosition =  menuPosition + 1;
         updateDisplay();
@@ -127,21 +185,30 @@ void loop() {
         menuPosition = menuPosition - 1;
         updateDisplay();
     }
-    String message = Serial.readString(); 
-    switch(message.charAt(0)) {
+    String message = Serial.readString();
+    
+      switch(message.charAt(0)) {
       case 'C': {
-        channelArray[elementIndex].letter = message.charAt(1);
-        channelArray[elementIndex].value = 0;
-        channelArray[elementIndex].minVal = 0;
-        channelArray[elementIndex].maxVal = 255;
-        elementIndex++;
-        sortLetters();
-        updateDisplay();
+        if (message.length() < 3) {
+          Serial.println("ERROR: " + message); 
+        } else {
+          channelArray[elementIndex].letter = message.charAt(1);
+          channelArray[elementIndex].value = 0;
+          channelArray[elementIndex].minVal = 0;
+          channelArray[elementIndex].maxVal = 255;
+          elementIndex++;
+          sortLetters();
+          updateDisplay();
+          
+        }
         break;
      
         
       }
       case 'V': {
+        if ((message.length() - 1) < 3) {
+      Serial.println("ERROR: " + message); 
+    } else {
         int index = getIndex(message.charAt(1));
         if (index == -1) {
           Serial.println("channel selected does not exist");
@@ -151,16 +218,81 @@ void loop() {
           
         }
         updateDisplay();
+      }
+        updateColour();
         break;
       }
       case 'N': {
-        
+        if ((message.length() - 1) < 3) {
+      Serial.println("ERROR: " + message); 
+    } else {
+        int index = getIndex(message.charAt(1));
+        if (index == -1) {
+          Serial.println("channel selected does not exist");
+        } else {
+          
+          channelArray[index].minVal = (message.substring(2,5)).toInt();
+          
       }
     }
+     updateColour();
+     break;
+    }
+    case 'X': {
+      if ((message.length() - 1) < 3) {
+      Serial.println("ERROR: " + message); 
+    } else {
+        int index = getIndex(message.charAt(1));
+        if (index == -1) {
+          Serial.println("channel selected does not exist");
+        } else {
+          
+          channelArray[index].maxVal = (message.substring(2,5)).toInt();
+          
+      }
+    }
+      updateColour();
+      break;
+    }
+    default: {
+      if (message != NULL) {
+        Serial.println("ERROR: " + message);
+      }
+    break;
+      
+      }
+      
+      
+    }
+    break;
    }
+   case SELECT: {
+     while (lcd.readButtons() == 1) {
+      int t = millis();
+      if (t > 1000) {
+        
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.setBacklight(5);
+        lcd.print("F124497");
+        
+      }
+       delay(1000);
+     }
+       lcd.clear();
+       updateDisplay();
+       updateColour();
+       state = MAIN;
+    
+     
+     break;
+        
+    }
+    }
+    
   }
+  
+
       
       
   
-
-}
